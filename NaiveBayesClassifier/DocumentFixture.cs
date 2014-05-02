70fb0ca06e0e5ce8fb0e397a4447f70bec1bb182
Andrei <andrei.bancioiu@gmail.com>
2014-05-01 17:10:45 -0700

    Sketched the VS project.

---
+++ NaiveBayesClassifier/DocumentFixture.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace NaiveBayesClassifier
+{
+    [TestFixture]
+    public class DocumentFixture
+    {
+        [Test]
+        public void DoesItWork()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
